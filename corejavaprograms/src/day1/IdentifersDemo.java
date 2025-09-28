5b1ab50d05d3f3d84ef0b72cc49da55d969ff469
Asus <Asus@monish2406>
2025-09-28 18:56:43 +0530

    commit

--- corejavaprograms/src/day1/IdentifersDemo.java
+++ corejavaprograms/src/day1/IdentifersDemo.java
@@ -4,6 +4,12 @@ public class IdentifersDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Aniket"; 
+		System.out.println("value is : "+ studentName);
+
 
 	}
 
---
+++ corejavaprograms/src/day3/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package day3.encapsulation;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Oopsconcept obj = new Oopsconcept();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+
+}
