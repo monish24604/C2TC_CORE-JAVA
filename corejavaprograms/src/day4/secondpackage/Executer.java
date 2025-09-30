feb2f354d468a517f5c5efa75bea5df0f6d0a497
Asus <Asus@monish2406>
2025-09-30 09:42:57 +0530

    commit

---
+++ corejavaprograms/src/day4/secondpackage/Executer.java
@@ -0,0 +1,20 @@
+package day4.secondpackage;
+
+import day4.firstpackage.Base;
+
+public class Executer {
+
+	public static void main(String[] args) {
+		
+				Base b1 = new Base();
+
+				
+
+				b1.methodPublic();
+				b1.varPublic = 41;
+				b1.methodPublic();
+
+
+	}
+
+}
